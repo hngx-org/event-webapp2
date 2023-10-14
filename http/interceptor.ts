e9f36080eed987b782b6f87269ec1ecbef998780
canhamzacode <abdulmuizzayo6@gmail.com>
2023-10-14 23:08:44 +0200

    autehntication

--- http/interceptor.ts
+++ http/interceptor.ts
@@ -3,9 +3,6 @@ import axios from "axios";
 // axios config for server
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-// const token = localStorage.getItem("token") || "";
-// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjMwODUyOGM2LTgyNmUtNDQyOS1iZjVhLTgxYTkxYmFkNGU3OCIsImlhdCI6MTY5NzMxMjQyNCwiZXhwIjoxNjk3Mzk4ODI0fQ.eDfBfOpY0YC5M09Vs1vIwLsvLfbpmjtEazFZxyII5CQ"
-
 const http = axios.create({
   baseURL: API_URL,
   timeout: 10000,
